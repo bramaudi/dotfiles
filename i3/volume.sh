b83f4ad60837d38625d956cef70ed7e904b2424e
Brama Udi <bramaudi@users.noreply.github.com>
2021-11-18 22:00:45 +0700

    Adjust volume notify

--- i3/volume.sh
+++ i3/volume.sh
@@ -21,11 +21,11 @@ function send_notification {
 #bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 if [ "$volume" = "0" ]; then
         icon_name="$HOME/.icons/Fluent/24/panel/audio-volume-muted.svg"
-$DIR/notify-send.sh "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
+$DIR/notify-send.sh "Volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" --replace=555
     else
 	if [  "$volume" -lt "10" ]; then
 	     icon_name="$HOME/.icons/Fluent/24/panel/audio-volume-low.svg"
-$DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
+$DIR/notify-send.sh "Volume""     " -i "$icon_name" --replace=555 -t 2000
     else
         if [ "$volume" -lt "30" ]; then
             icon_name="$HOME/.icons/Fluent/24/panel/audio-volume-low.svg"
@@ -38,9 +38,9 @@ $DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
         fi
     fi
 fi
-bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
+#bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 # Send the notification
-$DIR/notify-send.sh "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
+$DIR/notify-send.sh "Volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" --replace=555
 
 }
 
